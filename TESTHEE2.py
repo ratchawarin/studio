24523d9d4671df3c2ea417523ad137ca80ec660b
ratchawarin <gurock.net@gmail.com>
2023-11-01 16:09:14 +0700

    editfirst commit

--- TESTHEE2.py
+++
@@ -1,9 +0,0 @@
-or row in data:
-                tree.insert("", "end", values=row)
-
-            number_of_ids = len(tree.get_children())
-            total_amount = number_of_ids * 800
-
-            # Display the total amount
-            total_label = Label(currentwindow, text=f"Total Amount: {total_amount} Baht", font=("DB HELVETHAICA X BD EXT", 20))
-            total_label.grid(row=1, column=0, padx=50, pady=20)
